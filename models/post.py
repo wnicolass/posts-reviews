26eb7586969c98a0519ae0e72a5c6bf62755329b
wnicolass <nicolaswgl99@gmail.com>
2023-03-12 20:14:27 +0000

    fix: missing length on varchar attr

--- models/post.py
+++ models/post.py
@@ -6,7 +6,7 @@ from sqlalchemy import (
     DateTime,
 )
 from sqlalchemy.orm import relationship
-from config.database import Base, SessionLocal
+from config.database import Base
 
 class Post(Base):
     __tablename__ = 'posts'
@@ -14,7 +14,7 @@ class Post(Base):
     id = Column(Integer, primary_key = True, autoincrement = True)
     title = Column(String(50), nullable = False)
     summary = Column(String(50), nullable = False)
-    content = Column(String, nullable = False)
+    content = Column(String(255), nullable = False)
     created_at = Column(DateTime, default = datetime.now())
 
     reviews = relationship('Review', back_populates = 'post')
