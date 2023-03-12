f971be71a5364d988333b366da5772becba4f861
wnicolass <nicolaswgl99@gmail.com>
2023-03-12 19:54:38 +0000

    feat: end database connection config

---
+++ config/database.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv(find_dotenv())
+
+engine = create_engine(url = os.getenv("CONNECTION_STRING"))
+SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
+Base = declarative_base()
+
+def create_metadata():
+    Base.metadata.drop_all(bind=engine)  
+    Base.metadata.create_all(bind=engine)
