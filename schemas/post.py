d00cf6440d4c6ff4171a29f69676ddb9e2544818
wnicolass <nicolaswgl99@gmail.com>
2023-03-13 23:19:12 +0000

    feat: add orm config

--- schemas/post.py
+++ schemas/post.py
@@ -1,9 +1,14 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class PostBase(BaseModel):
     title: str
     summary: str | None = None
     content: str
+
+    class Config:
+        orm_mode = True
     
 class PostResult(PostBase):
     id: int
+    created_at: datetime
