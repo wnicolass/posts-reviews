72ac18934d8fbe78f46a43526c2d3016001a86b4
wnicolass <nicolaswgl99@gmail.com>
2023-03-16 20:58:33 +0000

    feat: create function to add a new review

--- routes/review_routes.py
+++ routes/review_routes.py
@@ -1,14 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status, Depends
+from sqlalchemy.orm import Session
 from schemas.review import ReviewBase, ReviewResult
+from utils.db_session import get_db_session
+from services import review_service
 
 router = APIRouter()
 
-@router.post("/reviews/{post_id}")
-async def create_review(review: ReviewBase, post_id: int) -> ReviewResult:
-    dummy_review = {
-        "id": 1,
-        "reviewer_id": review.reviewer_id,
-        "rating": review.rating,
-        "content": review.content
-    }
-    return dummy_review
+@router.post("/reviews/{post_id}", status_code = status.HTTP_201_CREATED)
+async def create_review(review: ReviewBase, post_id: int, session: Session = Depends(get_db_session)) -> ReviewResult:
+    if not post_id:
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST, 
+            detail = {'error': 'Missing id.'}
+        )
+    
+    new_review = review_service.create_review(session, review, post_id)
+    print(new_review)
+    if not new_review:
+        raise HTTPException(
+            status_code = status.HTTP_500_INTERNAL_SERVER_ERROR, 
+            detail = {'error': 'Something went wrong while connecting to the database.'}
+        )
+    
+    return new_review
