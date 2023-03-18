9481415f84e5255697754e117c657c1936a7067c
wnicolass <nicolaswgl99@gmail.com>
2023-03-18 19:53:17 +0000

    feat: add function to delete a review

--- services/review_service.py
+++ services/review_service.py
@@ -20,11 +20,19 @@ def create_review(session: Session, review_sch: ReviewBase, post_id: int) -> mod
 def get_all_reviews(session: Session, post_id: int) -> List[model.Review]:
     return session.query(model.Review).where(model.Review.post_id == post_id).all()
 
+def get_review_by_id(session: Session, review_id: int) -> model.Review:
+    return session.query(model.Review).filter(model.Review.id == review_id).one_or_none()
+
 def update_review(session: Session, review_id: int, new_data: ReviewBase) -> model.Review:
-    db_review = session.query(model.Review).filter(model.Review.id == review_id).one_or_none()
+    db_review = get_review_by_id(session, review_id)
     db_review.rating = new_data.rating
     db_review.content = new_data.content
     
     session.commit()
     
     return db_review
+
+def delete_review(session: Session, review_id) -> None:
+    review_to_delete = get_post_by_id(session, review_id)
+    session.delete(review_to_delete)
+    session.commit()
